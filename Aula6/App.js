4d256ca9ae1a963b4a781c4bf20f2a0f31d1d87b
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-03-12 09:54:57 -0300

    mobile

--- Aula6/App.js
+++ Aula6/App.js
@@ -1,58 +1,69 @@
-import React, {Component} from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Picker } from "@react-native-picker/picker";
-
-// Aula 6 - Picker
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+ 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pizza: 0,
+      pizza: 0, // default selected pizza index
       pizzas: [
-        {key: 1, nome: 'Calabreza', valor: 50.00},
-        {key: 2, nome: 'Muzzarela', valor: 49.00},
-        {key: 3, nome: '4 Queijos', valor: 35.00},
-        {key: 4, nome: 'Catupiry', valor: 39.99}
+        { key: 1, nome: 'Calabreza', valor: 50.00 },
+        { key: 2, nome: 'Muzzarela', valor: 49.00 },
+        { key: 3, nome: '4 Queijos', valor: 35.00 },
+        { key: 4, nome: 'Catupiry', valor: 39.99 },
+        { key: 5, nome: 'Portuguesa', valor: 40.00 },
       ]
     };
   }
+ 
   render() {
-    let pizzaItem = this.state.pizzas.map((v, k)=> {return <Picker.Item key={k} value={v} label={v.nome}/>})
-    return(
+    let pizzaItem = this.state.pizzas.map((v, k) => {
+      return <Picker.Item key={k} value={k} label={v.nome} />;
+    });
+ 
+    return (
       <View style={estilos.container}>
         <Text style={estilos.logo}>Menu Pizza</Text>
-        <Picker selectedValue={1} onValueChange={(ItemValue, ItemIndex)=> this.setState({pizzas: ItemValue})}> 
-          <Picker.Item label="Escolha sua Pizza" enabled={false}/>
+        <Picker
+          selectedValue={this.state.pizza}
+          onValueChange={(itemValue) => this.setState({ pizza: itemValue })}
+        >
+          <Picker.Item label='Escolha aqui sua pizza' enabled={false} />
           {pizzaItem}
         </Picker>
-        <Text style={estilos.pizzas}>Você escolheu a pizza de: {this.state.pizzas[this.state.pizza].nome}</Text>
-        <Text style={estilos.valor}>R$: 50.00</Text>
+        <Text style={estilos.pizza}>Você escolheu pizza de: {this.state.pizzas[this.state.pizza].nome}</Text>
+        <Text style={estilos.valor}>R${this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
       </View>
     );
   }
 }
-
+ 
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
+    marginTop: 50
   },
+ 
   logo: {
     textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold'
   },
-  pizzas: {
+ 
+  pizza: {
     textAlign: 'center',
     fontSize: 15,
     marginTop: 10
   },
+ 
   valor: {
     textAlign: 'center',
     fontSize: 25,
-    marginTop: 10,
+    marginTop: 15,
     fontWeight: 'bold'
   }
 });
-
-export default App
+ 
+export default App;
+ 
