095524338b0f51e28df038c4db8ca455a5959ccd
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-03-17 10:30:34 -0300

    mobile

--- Aula9/App.js
+++ Aula9/App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Aula 9 - AsyncStorage
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entrada: "",
+      nome: ""
+    };
+    this.gravarNome = this.gravarNome.bind(this);
+  }
+
+  async componentDidMount() {
+    await AsyncStorage.getItem("nome").then((value)=> {
+      this.setState({nome: value});
+    });
+  }
+  async componentDidUpdate(_, prevState) {
+    if(prevState !== this.state.nome) {
+      await AsyncStorage.setItem('nome', this.state.nome)
+    }
+  }
+
+  gravarNome() {
+    const{entrada} = this.state;
+    this.setState({
+      nome: this.state.entrada
+    });
+    AsyncStorage.setItem("nome", entrada);
+    alert('Salvo com sucesso')
+    Keyboard.dismiss();
+  }
+
+  render() {
+    return (
+      <View style={estilos.container}>
+        <View style={estilos.viewEntrada}>
+          <TextInput style={estilos.input} 
+          value={this.state.entrada} 
+          onChangeText={(texto)=> this.setState({entrada: texto})}
+          ></TextInput>
+          <TouchableOpacity onPress={this.gravarNome}>
+            <Text style={estilos.botao}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={estilos.texto}>{this.state.nome}</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    alignItems: 'center'
   },
+  viewEntrada: {
+    flexDirection: "row",
+  },
+  input: {
+    width: 350,
+    height: 40,
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 10
+  },
+  botao: {
+    backgroundColor: '#000',
+    color: 'white',
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+  },
+  texto: {
+    fontSize: 20,
+    marginTop: 10
+  }
 });
+
+export default App;
