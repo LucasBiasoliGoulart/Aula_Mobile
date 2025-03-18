f0f89e1a61f7cbcd7c2f1dd76b47cd025415c406
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-03-18 11:33:20 -0300

    mobile

--- Aula9/App.js
+++ Aula9/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Modal } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Modal, Image, TouchableOpacity } from 'react-native';
 import Entrar from "./src/Entrar.js";
 
-// Aula 10 - Modal
+// Aula - Modal
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      visibModal: false
+      visibModal: false,
+      precoA: 0,
+      precoG: 0,
+      resultado: ''
     };
     this.entrar = this.entrar.bind(this);
     this.sair = this.sair.bind(this);
   }
 
   entrar() {
+    this.calcular();
     this.setState({visibModal: true})
   }
 
@@ -21,13 +25,57 @@ class App extends Component {
     this.setState({visibModal: estado})
   }
 
+  // Calcular
+  calcular() {
+    const { precoA, precoG } = this.state;
+    if(precoA > 0 && precoG > 0) {
+      const resultado = (precoA / precoG) > 0.7 ? 'Álcool é melhor' : 'Gasolina é melhor';
+      this.setState({ resultado });
+    }
+  }
+
   render() {
+    let icone = 'https://cdn-icons-png.flaticon.com/512/1986/1986894.png'
     return (
       <View style={estilos.container}>
-        <Button title='Entrar' onPress={this.entrar}></Button>
+        <View style={estilos.banner}>
+          <Image source={{uri:icone}} style={estilos.icone}></Image>
+          <Text style={estilos.titulo}>Qual a melhor opção ?</Text>
+        </View>
+        <View style={estilos.caixa}>
+          <Text style={estilos.texto}>Alcool (preço por litro):</Text>
+          <TextInput 
+            style={estilos.input} 
+            keyboardType='numeric' 
+            onChangeText={(text)=> this.setState({ precoA: parseFloat(text) })}
+            value={this.state.precoA ? this.state.precoA.toString() : ''}
+          />
+        </View>
+        <View style={estilos.caixa}>
+          <Text style={estilos.texto}>Gasolina (preço por litro):</Text>
+          <TextInput 
+            style={estilos.input}
+            keyboardType='numeric'
+            onChangeText={(text)=> this.setState({ precoG: parseFloat(text) })}
+            value={this.state.precoG ? this.state.precoG.toString() : ''}
+          />
+        </View>
+        <TouchableOpacity style={estilos.botao} onPress={this.entrar}>
+          <Text style={estilos.texto}>Calcular</Text>
+        </TouchableOpacity>
         <Modal visible={this.state.visibModal} animationType='fade' transparent={true}>
-          <View style={{ marginTop: 15, flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10}}>
-            <Entrar fechar={()=> this.sair(false)}></Entrar>
+          <View style={{ 
+              flex: 1, 
+              alignItems: 'center', 
+              justifyContent: 'center', 
+              padding: 10 
+            }}>
+            <Entrar 
+              precoA={this.state.precoA} 
+              precoG={this.state.precoG}
+              resultado={this.state.resultado}
+              fechar={()=> this.sair(false)}
+            ></Entrar>
           </View>
         </Modal>
       </View>
@@ -38,9 +86,47 @@ class App extends Component {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 70,
     alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#292929',
+    padding: 10
   },
+  banner: {
+    width: '100%',
+    padding: 10,
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginTop: 100,
+    marginBottom: 30
+  },
+  icone: {
+    width: 200,
+    height: 200,
+  },
+  titulo: {
+    fontSize: 25,
+    color: 'white',
+  },
+  texto: {
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 5
+  },
+  caixa: {
+    width: '100%',
+    marginBottom: 17
+  },
+  input: {
+    borderWidth: 1,
+    backgroundColor: 'white'
+  },
+  botao: {
+    width: '100%',
+    padding: 10,
+    alignItems: 'center',
+    backgroundColor: 'red',
+    borderRadius: 5
+  }
 });
 
 export default App;
--- Aula9/src/Entrar.js
+++ Aula9/src/Entrar.js
@@ -1,21 +1,81 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-// Aula 10 - Modal
+// Aula - Modal
 class Entrar extends Component {
   render() {
+    const { precoA, precoG, resultado } = this.props;
+    let icone = 'https://cdn-icons-png.flaticon.com/512/5984/5984310.png'
     return (
-      <View style={{ 
-        backgroundColor: '#292929', 
-        width: '100%', 
-        height: 350, 
-        borderRadius: 20
-      }}>
-        <Text style={{ color: "#fff", fontSize: 25, padding: 15, textAlign: 'center' }}>Seja Bem Vindo !!</Text>
-        <Button title='Sair' onPress={() => this.props.fechar()} />
+      <View style={estilos.carta}>
+        <View style={estilos.banner}>
+          <Image source={{uri:icone}} style={estilos.icone}></Image>
+          <Text style={estilos.titulo}>{resultado}</Text>
+        </View>
+        <View style={estilos.caixa}>
+          <Text style={estilos.subtitulo}>Com os preços:</Text>
+          <Text style={estilos.textos}>Álcool: R${precoA}</Text>
+          <Text style={estilos.textos}>Gasolina: R${precoG}</Text>
+        </View>
+        <TouchableOpacity style={estilos.botao} onPress={() => this.props.fechar()}>
+          <Text style={estilos.texto}>Sair</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const estilos = StyleSheet.create({
+  carta: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    backgroundColor: '#141414',
+    padding: 10
+  },
+  banner: {
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  icone: {
+    width: 200,
+    height: 200,
+  },
+  titulo: {
+    color: 'lime',
+    fontSize: 30,
+    padding: 5,
+    textAlign: 'center'
+  },
+  subtitulo: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  caixa: {
+    marginTop: 20,
+    marginBottom: 20,
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  textos: {
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 5
+  },
+  texto: {
+    color: 'white',
+    fontSize: 20
+  },
+  botao: {
+    width: '100%',
+    padding: 10,
+    alignItems: 'center',
+    backgroundColor: 'red',
+    borderRadius: 5
+  }
+});
 
 export default Entrar;
