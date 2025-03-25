f5e16217c5cfc1cc235bf410ab46c80822890d7b
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-03-25 10:37:23 -0300

    mobile

--- Aula13_Moedas/App.js
+++ Aula13_Moedas/App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.card}>
+        <Text style={styles.titulo}>Selecione sua Moeda</Text>
+        <Picker style={styles.pickerContainer}>
+          <Picker.Item label='USD'/>
+        </Picker>
+        <Text style={styles.titulo}>Digite uma valor para converter em (R$)</Text>
+        <TextInput style={styles.input} keyboardType='numeric'></TextInput>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}>Converter</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +23,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#292929'
   },
+  card: {
+    width: '95%',
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: '#FFF',
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 17
+  },
+  pickerContainer: {
+    marginBottom: 5,
+    marginTop: 10,
+    backgroundColor: '#E4E4E4',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#E4E4E4',
+    marginTop: 5,
+    marginBottom: 10,
+    borderRadius: 5
+  },
+  botao: {
+    width: '100%',
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#34C25E'
+  },
+  textoBotao: {
+    textAlign: 'center',
+    fontSize: 20
+  }
 });
