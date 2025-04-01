53af30a2e5c47ab20cbe25db6023d01c6d4c2a51
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-04-01 08:02:20 -0300

    mobile

--- Aula16_Cronometro/App.js
+++ Aula16_Cronometro/App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: 'Iniciar'
+    };
+    this.iniciar = this.iniciar.bind(this);
+    this.limpar = this.limpar.bind(this);
+
+    this.time = null;
+  }
+
+  // Iniciar o cronometro
+  iniciar() {
+    if(this.time != null) { 
+      clearInterval(this.time);
+      this.time = null
+      this.setState({botao: 'Iniciar'});
+    }else {
+      this.time = setInterval(()=> {
+      this.setState({numero: this.state.numero + 0.1})
+    }, 100);
+      this.setState({botao: 'Parar'});
+    }
+  }
+
+  // Limpar o cronometro
+  limpar() {
+    clearInterval(this.time);
+    this.time = null
+    this.setState({
+      numero: 0,
+      botao: 'Iniciar'
+    });
+  }
+  render() {
+    return(
+      <View style={styles.container}>
+        <Image source={require("./src/cronometro.png")}></Image>
+        <Text style={styles.textoTimer}>{this.state.numero.toFixed(1)}</Text>
+        <View style={styles.areaBtn}>
+          <TouchableOpacity style={styles.botao} onPress={this.iniciar}>
+            <Text style={styles.btnTexto}>{this.state.botao}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botao} onPress={this.limpar}>
+            <Text style={styles.btnTexto}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00eeff'
   },
+  textoTimer: {
+    color: "#FFF",
+    fontSize: 50,
+    marginTop: -160
+  },
+  areaBtn: {
+    flexDirection: 'row',
+    marginTop: 100
+  },
+  botao: {
+    width: 150,
+    height: 50,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#FFF",
+    margin: 10
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000'
+  }
 });
+
+export default App;
