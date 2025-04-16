91195a55c508109fa452ec50e4754bc870d6fe66
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-04-16 10:33:07 -0300

    mobile

--- Aula23/App.js
+++ Aula23/App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { doc, getDoc, onSnapshot, setDoc, collection } from 'firebase/firestore';
+import { db } from './src/firebaseConnection';
 
-export default function App() {
+// Aula 23 - Banco
+export default function Fire() {
+  const[nome, setNome] = useState('Carregando...');
+  const[idade, setIdade] = useState("")
+  const[cargo, setCargo] = useState("");
+
+  useEffect(()=> {
+    async function getDados() {
+      /*const docref = doc(db, 'users', '2')
+      getDoc(docref)
+      .then((snapshot)=> {
+        console.log(snapshot.data());
+        setNome(snapshot.data()?.nome);
+        setCargo(snapshot.data().cargo)
+        .catch(()=> {
+          console.log(err);
+        })
+      });*/
+      onSnapshot(doc(db, "users", "2"), (doc)=> {
+        setNome(doc.data()?.nome)
+        setIdade(doc.data()?.idade)
+        setCargo(doc.data()?.cargo)
+      })
+    }
+    getDados();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>FireBase</Text>
+      <Text>Nome: {nome}</Text>
+      <Text>Idade: {idade}</Text>
+      <Text>Cargo: {cargo}</Text>
+      <TouchableOpacity style={styles.botao} onPress={registraDados}>
+        <Text style={{ color: "#FFF", fontSize: 18 }}>Click</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +50,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  botao: {
+    width: 170,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10
+  }
 });
