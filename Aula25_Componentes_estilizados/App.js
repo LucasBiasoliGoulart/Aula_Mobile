4529351679d073d72f1e53af6bc7a092f33397e6
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-05-05 11:35:52 -0300

    mobile

---
+++ Aula25_Componentes_estilizados/App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+//import styled from 'styled-components';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Componented Estilizados</Text>
+    </View>
+  );
+}
