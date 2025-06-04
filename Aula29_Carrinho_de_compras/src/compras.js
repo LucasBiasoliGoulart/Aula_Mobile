b5b5acc7c65233221c6c56117eaa44a2eb3c9cba
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-06-04 11:48:58 -0300

    mobile

--- Aula29_Carrinho_de_compras/src/compras.js
+++ Aula29_Carrinho_de_compras/src/compras.js
@@ -1,12 +1,55 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 import Icone from '@expo/vector-icons/Feather';
+import { useState } from 'react';
 
-export default function App() {
+export default function Compras() {
+  const route = useRoute();
+  const { produtos } = route.params || {produtos: []}
+  const[quantidade, setQuantidade] = useState({});
+
+  // Função para aumentar a quantidade
+  const aumentarQuantidade = (index) => {
+    setQuantidade(prev => ({...prev, [index]: (prev[index] || 0) + 1}));
+  }
+
+  // Função para diminuir a quantidade
+  const diminuirQuantidade = (index) => {
+    setQuantidade(prev => ({...prev, [index]: (prev[index] > 0 ? prev[index] - 1 : 0)}));
+  }
+
+  // Filtra produtos que tem quantidade 0
+  const produtosComQuantidade = produtos.filter((_, index)=> (quantidade[index] || 0) > 0);
+
+  // Função para calcular os preços * quantidade dos produtos
+  const total = produtosComQuantidade.reduce((acc, produto, index) => {
+    const originalIndex = produtos.indexOf(produto);
+    const qtd = quantidade[originalIndex] || 0;
+    return acc + (produto.preco * qtd);
+  }, 0);
   return (
     <View style={styles.container}>
-      <View style={styles.menu}>
-        <Text style={styles.titulo}>Compras</Text>
-      </View>
+      {produtosComQuantidade.map((item, index)=> {
+        const originalIndex = produtos.indexOf(item);
+        return (
+          <View style={{margin: 10}}>
+            <View style={styles.card} key={index}>
+              <Text style={{fontSize: 20}}>{item.nome}</Text>
+              <Text style={{fontSize: 16, color: '#2F9F3A'}}>R$: {item.preco.toFixed(2)}</Text>
+            </View>
+            <View style={styles.barra}>
+              <TouchableOpacity style={styles.botao} activeOpacity={0.7} onPress={()=> diminuirQuantidade(originalIndex)}>
+                <Icone name='minus' size={20}/>
+              </TouchableOpacity>
+              <Text style={{fontSize: 15}}>{quantidade[index] || 0}</Text>
+              <TouchableOpacity style={styles.botao} activeOpacity={0.7} onPress={()=> aumentarQuantidade(originalIndex)}>
+                <Icone name='plus' size={20}/>
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+      })}
+    <Text style={styles.texto}>Total R$: {total.toFixed(2)}</Text>
     </View>
   );
 }
@@ -16,16 +59,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  menu: {
-    width: '100%',
-    height: 100,
+  card: {
+    padding: 10,
     alignItems: 'center',
-    justifyContent: 'space-between',
     flexDirection: 'row',
-    padding: 15
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
   },
-  titulo: {
-    fontSize: 22,
-    fontWeight: 'bold'
+  barra: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 10,
+    gap: 10
+  },
+  botao: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'dodgerblue'
+  },
+  texto: {
+    margin: 10,
+    fontSize: 20
   }
 });
--- Aula29_Carrinho_de_compras/src/home.js
+++ Aula29_Carrinho_de_compras/src/home.js
@@ -1,21 +1,44 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Icone from '@expo/vector-icons/Feather';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
-export default function App() {
+export default function Home() {
+  const[contador, setContador] = useState(0);
+  const[produtosAdicionados, setProdutosAdicionados] = useState([]);
+
+  // Função para adicionar o produto
+  const adicionarProduto = (produto) => {
+    setContador(contador + 1);
+    setProdutosAdicionados([...produtosAdicionados, produto])
+  }
+
+  // Função de navegação
+  const navegacao = useNavigation();
+  function compra() {
+    navegacao.navigate('Compras', {
+      produtos: produtosAdicionados
+    });
+  }
   return (
     <View style={styles.container}>
       <View style={styles.menu}>
         <Text style={styles.titulo}>Lista de Compras</Text>
-        <TouchableOpacity activeOpacity={0.7}>
+        <TouchableOpacity activeOpacity={0.7} onPress={compra}>
             <Icone name='shopping-cart' size={30}/>
         </TouchableOpacity>
+        {contador > 0 && (
+          <View style={styles.numero}>
+            <Text style={{fontSize: 15, color: '#fff'}}>{contador}</Text>
+          </View>
+        )}
       </View>
       <View style={styles.card}>
         <View style={{padding: 10}}>
             <Text style={{fontSize: 20, fontWeight: 'bold'}}>Macarrão</Text>
             <Text style={{fontSize: 16, marginTop: 5, color: '#2F9F3A'}}>R$: 40,00</Text>
         </View>
-        <TouchableOpacity style={styles.botao} activeOpacity={0.7}>
+        <TouchableOpacity style={styles.botao} activeOpacity={0.7} onPress={()=> adicionarProduto({nome: 'Macarrão', preco: 40.00})}>
             <Icone name='plus' size={25}/>
         </TouchableOpacity>
       </View>
@@ -24,16 +47,34 @@ export default function App() {
             <Text style={{fontSize: 20, fontWeight: 'bold'}}>Leite</Text>
             <Text style={{fontSize: 16, marginTop: 5, color: '#2F9F3A'}}>R$: 27,00</Text>
         </View>
-        <TouchableOpacity style={styles.botao} activeOpacity={0.7}>
+        <TouchableOpacity style={styles.botao} activeOpacity={0.7} onPress={()=> adicionarProduto({nome: 'Leite', preco: 27.00})}>
             <Icone name='plus' size={25}/>
         </TouchableOpacity>
       </View>
       <View style={styles.card}>
         <View style={{padding: 10}}>
-            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Caixa de suco</Text>
-            <Text style={{fontSize: 16, marginTop: 5, color: '#2F9F3A'}}>R$: 10,00</Text>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Hamburguer</Text>
+            <Text style={{fontSize: 16, marginTop: 5, color: '#2F9F3A'}}>R$: 30,00</Text>
         </View>
-        <TouchableOpacity style={styles.botao} activeOpacity={0.7}>
+        <TouchableOpacity style={styles.botao} activeOpacity={0.7} onPress={()=> adicionarProduto({nome: 'Hamburguer', preco: 30.00})}>
+            <Icone name='plus' size={25}/>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.card}>
+        <View style={{padding: 10}}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Feijão</Text>
+            <Text style={{fontSize: 16, marginTop: 5, color: '#2F9F3A'}}>R$: 25,00</Text>
+        </View>
+        <TouchableOpacity style={styles.botao} activeOpacity={0.7} onPress={()=> adicionarProduto({nome: 'Feijão', preco: 25.00})}>
+            <Icone name='plus' size={25}/>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.card}>
+        <View style={{padding: 10}}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Fanta Laranja</Text>
+            <Text style={{fontSize: 16, marginTop: 5, color: '#2F9F3A'}}>R$: 9,00</Text>
+        </View>
+        <TouchableOpacity style={styles.botao} activeOpacity={0.7} onPress={()=> adicionarProduto({nome: 'Fanta Laranja', preco: 9.00})}>
             <Icone name='plus' size={25}/>
         </TouchableOpacity>
       </View>
@@ -59,19 +100,30 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   card: {
-    margin: 7,
+    margin: 5,
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    borderRadius: 5,
-    borderWidth: 1
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
   },
   botao: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     marginRight: 10,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'dodgerblue'
+  },
+  numero: {
+    width: 24,
+    height: 24,
+    position: 'absolute',
+    backgroundColor: '#DC3030',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    left: 340,
+    top: 45
   }
 });
